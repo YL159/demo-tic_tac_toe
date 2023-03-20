88959aa1f187ae4a68ddec6c2032eda81df96430
sea_melan <yang159@my.yorku.ca>
2023-03-19 20:28:36 -0400

    A demo tic_tac_toe in React

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { Component, useState } from "react";
+
+function Square({ value, onSquareClick }) {
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
+
+export function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        onPlay(nextSquares);
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    const Row = ({ i }) => {
+        return (
+            <div className="board-row">
+                {[i * 3, i * 3 + 1, i * 3 + 2].map((t) => (
+                    <Square
+                        value={squares[t]}
+                        onSquareClick={() => handleClick(t)}
+                        key={t}
+                    />
+                ))}
+            </div>
+        );
+    };
+
+    return (
+        <React.Fragment>
+            <div className="status">{status}</div>
+            {[0, 1, 2].map((r) => (
+                <Row i={r} key={r} />
+            ))}
+        </React.Fragment>
+    );
+}
+
+export default function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory([nextHistory]);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+            description = "Go to move #" + move;
+        } else {
+            description = "Go to game start";
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    xIsNext={xIsNext}
+                    squares={currentSquares}
+                    onPlay={handlePlay}
+                />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
